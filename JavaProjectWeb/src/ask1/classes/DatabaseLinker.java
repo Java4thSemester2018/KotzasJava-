a01b65262fc993e60e024b888ef670c4b9b44faa
johnythefoxGR <john.al.vra@hotmail.com>
2018-05-11 13:43:04 +0300

    added part

--- JavaProjectWeb/src/ask1/classes/DatabaseLinker.java
+++ JavaProjectWeb/src/ask1/classes/DatabaseLinker.java
@@ -45,4 +45,39 @@ public class DatabaseLinker {
 		}
 		return false;
 	}
+	public static ResultSet GetCourses(String Name) {
+		int count=0;
+		Connection c=null;
+		Statement stmt=null;
+		ResultSet rs=null;
+		try{
+			c= DriverManager.getConnection("jdbc:postgresql://localhost/postgres","kotz101","qwerty");
+			System.out.println("Opened database successfully");
+			stmt=c.createStatement();
+			rs = stmt.executeQuery("SELECT * FROM Courses");
+			return rs;
+		}
+		catch(Exception Ae){
+			System.out.println("ERROR: "+Ae.getMessage());
+		}
+		return rs;
+	}
+	public static ResultSet GetCoursesAndProfessors(String Name) {
+		int count=0;
+		Connection c=null;
+		Statement stmt=null;
+		ResultSet rs=null;
+		try{
+			c= DriverManager.getConnection("jdbc:postgresql://localhost/postgres","kotz101","qwerty");
+			System.out.println("Opened database successfully");
+			stmt=c.createStatement();
+			rs = stmt.executeQuery("Select courses.*, users.name , users.surname from professors,professors_courses,courses,users\n" + 
+					"where (professors.user_id = users.userid) and (professors_courses.professorafm = professors.professorafm) and (professors_courses.course_id = courses.courseid)");
+			return rs;
+		}
+		catch(Exception Ae){
+			System.out.println("ERROR: "+Ae.getMessage());
+		}
+		return rs;
+	}
 }
